b3766ca05a8edd93f384637fb55ac17720c18926
saqib25 <52543072+saqib25@users.noreply.github.com>
2020-07-12 20:51:04 +0500

    added files

---
+++ src/component/Navbar.js
@@ -0,0 +1,116 @@
+// import React from 'react';
+import { Link } from 'react-router-dom';
+
+// function NavBar() {
+
+//   return (
+// 	<div>
+//         <Link to="/"> Home </Link>
+//         <Link to="/about"> About </Link>
+//         <Link to="/product"> Product </Link>
+// 	</div>
+//   );
+// }
+
+// export default NavBar;
+
+import React from 'react';  
+import { makeStyles, withTheme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+
+import FormGroup from '@material-ui/core/FormGroup';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  link:{
+    padding: 10,
+    color: 'white',
+    textDecoration: "none"
+  }
+
+}));
+
+export default function MenuAppBar() {
+  const classes = useStyles();
+  const [auth, setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleChange = (event) => {
+    setAuth(event.target.checked);
+  };
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        
+        <Toolbar>
+        <div className={classes.navcontainer}>
+         <Link to="/" className={classes.link}> Home </Link>
+         <Link to="/about" className={classes.link}> About </Link>
+         <Link to="/product" className={classes.link}> Product </Link>
+         </div>
+          {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu"> */}
+            {/* <MenuIcon /> */}
+          {/* </IconButton> */}
+          <Typography variant="h6" className={classes.title}>
+            Photos
+          </Typography>
+          {auth && (
+            <div>
+              <IconButton
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+                className={classes.ic}
+              >
+                <ShoppingCartIcon   />
+              </IconButton>
+              {/* <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={open}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+              </Menu> */}
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
